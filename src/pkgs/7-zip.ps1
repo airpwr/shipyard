6b8f851203209c390cf8958899a098657da84c54
Troy Southard <troy.southard@us.af.mil>
2022-01-17 15:54:30 -0600

    out null

--- src/pkgs/7-zip.ps1
+++ src/pkgs/7-zip.ps1
@@ -9,7 +9,7 @@ function global:Install-PwrPackage {
 	Invoke-WebRequest -UseBasicParsing 'https://www.7-zip.org/a/7z2107-x64.exe' -OutFile $z
 	Invoke-WebRequest -UseBasicParsing 'https://www.7-zip.org/a/7za920.zip' -OutFile "$env:temp\7z.zip"
 	Expand-Archive "$env:temp\7z.zip" "$env:temp\7z"
-	& "$env:temp\7z\7za.exe" x -o'\pkg' $z
+	& "$env:temp\7z\7za.exe" x -o'\pkg' $z | Out-Null
 	Write-PackageVars @{
 		env = @{
 			path = (Get-ChildItem -Path '\pkg' -Recurse -Include '7z.exe' | Select-Object -First 1).DirectoryName
