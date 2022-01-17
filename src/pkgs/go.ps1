2178671cc2ffbd0f5ba073503c15295d7563567f
Troy Southard <troy.southard@us.af.mil>
2022-01-17 09:48:23 -0600

    add go package

---
+++ src/pkgs/go.ps1
@@ -0,0 +1,36 @@
+$global:PwrPackageConfig = @{
+	Name = 'go'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'golang'
+		Repo = 'go'
+		TagPattern = '^go([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Latest = Get-GitHubTag @Params
+	$PwrPackageConfig.UpToDate = -not $Latest.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Latest.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Tag = $Latest.name
+	$AssetName = "${$Tag}.windows-amd64.zip"
+	$Params = @{
+		AssetName = $AssetName
+		AssetIdentifier = $Tag
+		AssetURL = "https://go.dev/dl/$AssetName"
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'go.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	pwr sh 'file:///\pkg'
+	go version
+	pwr exit
+}
