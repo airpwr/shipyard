f17288ad2a0fac52713deb7cfe24849ce3b6c7c0
Troy Southard <troy.southard@us.af.mil>
2022-01-17 13:36:10 -0600

    add pwr package

---
+++ src/pkgs/pwr.ps1
@@ -0,0 +1,32 @@
+$global:PwrPackageConfig = @{
+	Name = 'pwr'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'airpwr'
+		Repo = 'airpwr'
+		TagPattern = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Latest = Get-GitHubTag @Params
+	$PwrPackageConfig.UpToDate = -not $Latest.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Latest.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Tag = $Latest.name
+	Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/airpwr/airpwr/$Tag/src/pwr.ps1" -OutFile '\pkg\pwr.ps1'
+	Write-PackageVars @{
+		env = @{
+			path = '\pkg'
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	pwr sh 'file:///\pkg'
+	if ((Get-Command pwr).Path -ne (Resolve-Path '\pkg\pwr.ps1').Path) {
+		Write-Error (Get-Command pwr).Path
+	}
+	pwr exit
+}
