3496f2a03d027df455b56122acd9f723e9213765
Troy Southard <troy.southard@us.af.mil>
2022-04-26 14:52:13 -0500

    add node v14

---
+++ src/pkgs/node14.ps1
@@ -0,0 +1,38 @@
+# End-of-Life is 2023-04-30. See https://nodejs.org/en/about/releases/
+$global:PwrPackageConfig = @{
+	Name = 'node'
+	Matcher = '^node-14\.'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'nodejs'
+		Repo = 'node'
+		TagPattern = '^v(14)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Latest = Get-GitHubTag @Params
+	$PwrPackageConfig.UpToDate = -not $Latest.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Latest.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Tag = $Latest.name
+	$AssetName = "node-$Tag-win-x64.zip"
+	$Params = @{
+		AssetName = $AssetName
+		AssetIdentifier = $Tag
+		AssetURL = "https://nodejs.org/dist/$Tag/$AssetName"
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'node.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	pwr sh 'file:///\pkg'
+	node --version
+	pwr exit
+}
