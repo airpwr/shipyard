d5f65559f548bbd5c83638f78a9f800ac4c50c99
Nick Little <nicklaus.little@gmail.com>
2022-07-02 12:05:00 -0500

    Add jre and jdk x86 configuration

--- src/pkgs/jre8.ps1
+++ src/pkgs/jre8.ps1
@@ -21,16 +21,37 @@ function global:Install-PwrPackage {
 		AssetURL = $Asset.URL
 	}
 	Install-BuildTool @Params
+	$Params_x86 = @{
+		AssetName = $Asset.Name.Replace('_x64_', '_x86-32_')
+		AssetURL = $Asset.URL.Replace('_x64_', '_x86-32_')
+		ToolDir = '\pkg\x86'
+	}
+	Install-BuildTool @Params_x86
 	Write-PackageVars @{
 		env = @{
-			java_home = (Split-Path (Get-ChildItem -Path '\pkg' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
-			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
+			java_home = (Split-Path (Get-ChildItem -Path '\pkg' -Exclude 'x86' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
+			path = (Get-ChildItem -Path '\pkg' -Exclude 'x86' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
+		}
+		amd64 = @{
+			env = @{
+				java_home = (Split-Path (Get-ChildItem -Path '\pkg' -Exclude 'x86' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
+				path = (Get-ChildItem -Path '\pkg' -Exclude 'x86' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
+			}
+		}
+		x86 = @{
+			env = @{
+				java_home = (Split-Path (Get-ChildItem -Path '\pkg\x86' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
+				path = (Get-ChildItem -Path '\pkg\x86' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
+			}
 		}
 	}
 }
 
 function global:Test-PwrPackageInstall {
 	pwr sh 'file:///\pkg'
+	java -version
+	pwr exit
+	pwr sh 'file:///\pkg<x86'
 	java -version
 	pwr exit
 }
