28f1d6db35fc961c93945a0832053f93317571e1
Nick Little <nicklaus.little@gmail.com>
2022-10-07 23:04:44 -0500

    Add chromium

---
+++ src/pkgs/chromium.ps1
@@ -0,0 +1,34 @@
+$global:PwrPackageConfig = @{
+	Name = 'chromium'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'ungoogled-software'
+		Repo = 'ungoogled-chromium-windows'
+		AssetPattern = 'ungoogled-chromium_.*_windows_x64\.zip$'
+		TagPattern = '^([0-9]+)\.([0-9]+)\.([0-9]+)(\.[0-9]+)(-.+)?$'
+	}
+	$Asset = Get-GitHubRelease @Params
+	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Asset.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Params = @{
+		AssetName = $Asset.Name
+		AssetURL = $Asset.URL
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'chrome.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	pwr sh 'file:///\pkg'
+	chrome --headless --disable-gpu --dump-dom https://github.com/ungoogled-software/ungoogled-chromium-windows/releases/latest | tee -Variable ChromeStatus
+	pwr exit
+}
