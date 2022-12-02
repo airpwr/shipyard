1effb9a6c409a0e6da269094246fca93acf50f10
Troy Southard <troy.southard@us.af.mil>
2022-12-02 15:34:48 -0500

    Add nats server

---
+++ src/pkgs/nats-server.ps1
@@ -0,0 +1,34 @@
+$global:PwrPackageConfig = @{
+	Name = 'nats-server'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'nats-io'
+		Repo = 'nats-server'
+		AssetPattern = 'nats-server-v.+-windows-amd64.zip'
+		TagPattern = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Asset = Get-GitHubRelease @Params
+	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Asset.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Params = @{
+		AssetName = $Asset.Name
+		AssetURL = $Asset.URL
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'nats-server.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	pwr sh 'file:///\pkg'
+	nats-server --version
+	pwr exit
+}
