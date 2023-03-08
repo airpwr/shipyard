a628625e919729b3c8af56f19d76cb2386b9c19e
Nick Little <nicklaus.little@us.af.mil>
2023-03-08 06:12:20 -0600

    Add gradle 8

---
+++ src/pkgs/gradle/gradle8.ps1
@@ -0,0 +1,38 @@
+$global:PwrPackageConfig = @{
+	Name = 'gradle'
+	Matcher = '^gradle-8\.'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'gradle'
+		Repo = 'gradle'
+		TagPattern = '^v(8)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Latest = Get-GitHubTag @Params
+	$PwrPackageConfig.UpToDate = -not $Latest.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Latest.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Tag = $Latest.name
+	$Version = $Tag.SubString(1)
+	if ($Version.EndsWith('.0')) {
+		$Version = $Version.SubString(0, $Version.Length - 2)
+	}
+	$AssetName = "gradle-$Version-bin.zip"
+	$Params = @{
+		AssetName = $AssetName
+		AssetURL = "https://services.gradle.org/distributions/$AssetName"
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'gradle.bat' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	Get-Content '\pkg\.pwr'
+}
