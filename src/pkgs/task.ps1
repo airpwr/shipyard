1b446f74b7accb5b6779068b519cb859a24655cf
Troy Southard <troy.southard@us.af.mil>
2023-10-13 11:27:57 -0500

    Add task

---
+++ src/pkgs/task.ps1
@@ -0,0 +1,34 @@
+$global:PwrPackageConfig = @{
+	Name = 'task'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'go-task'
+		Repo = 'task'
+		AssetPattern = 'task_windows_amd64\.zip'
+		TagPattern = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Asset = Get-GitHubRelease @Params
+	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Asset.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Params = @{
+		AssetName = $Asset.Name
+		AssetURL = $Asset.URL
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'task.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	pwr exec 'file:///\pkg' {
+		task --version
+	}
+}
