7a6456e3c57c8e05e2a5155d67a664677e64cedc
Troy Southard <troy.southard@us.af.mil>
2024-03-20 08:49:59 -0500

    Add zstd

---
+++ src/pkgs/zstd.ps1
@@ -0,0 +1,34 @@
+$global:PwrPackageConfig = @{
+	Name = 'zstd'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'facebook'
+		Repo = 'zstd'
+		AssetPattern = 'zstd-v+-win64.zip'
+		TagPattern = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Asset = Get-GitHubRelease @Params
+	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Asset.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Params = @{
+		AssetName = $Asset.Name
+		AssetURL = $Asset.URL
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'zstd.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	airpower exec 'file:///\pkg' {
+		zstd --version
+	}
+}
