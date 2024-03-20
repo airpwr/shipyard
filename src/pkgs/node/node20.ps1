d028a1f7708580cb3c75937e5e219ce243f9c430
Troy Southard <troy.southard@us.af.mil>
2024-03-20 09:04:24 -0500

    Add node 20

--- src/pkgs/node/node20.ps1
+++ src/pkgs/node/node20.ps1
@@ -1,14 +1,14 @@
-# End-of-Life is 2023-04-30. See https://nodejs.org/en/about/releases/
+# End-of-Life is 2026-04-30. See https://nodejs.org/en/about/releases/
 $global:PwrPackageConfig = @{
 	Name = 'node'
-	Matcher = '^node-14\.'
+	Matcher = '^node-20\.'
 }
 
 function global:Install-PwrPackage {
 	$Params = @{
 		Owner = 'nodejs'
 		Repo = 'node'
-		TagPattern = '^v(14)\.([0-9]+)\.([0-9]+)$'
+		TagPattern = '^v(20)\.([0-9]+)\.([0-9]+)$'
 	}
 	$Latest = Get-GitHubTag @Params
 	$PwrPackageConfig.UpToDate = -not $Latest.Version.LaterThan($PwrPackageConfig.Latest)
