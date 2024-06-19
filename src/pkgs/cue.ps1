35bdc24030e61dbd5fe7ad4a27d1bb423e61d034
Troy Southard <troy.southard@us.af.mil>
2024-06-19 09:07:08 -0500

    Add cue

---
+++ src/pkgs/cue.ps1
@@ -0,0 +1,34 @@
+$global:PwrPackageConfig = @{
+	Name = 'cue'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'cue-lang'
+		Repo = 'cue'
+		AssetPattern = '^cue_v.+_windows_amd64\.zip$'
+		TagPattern = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Asset = Get-GitHubRelease @Params
+	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Asset.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Params = @{
+		AssetName = $Asset.Name
+		AssetURL  = $Asset.URL
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = (Get-ChildItem -Path '\pkg' -Recurse -Include 'cue.exe' | Select-Object -First 1).DirectoryName
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	Airpower exec 'file:///\pkg' {
+		cue version
+	}
+}
