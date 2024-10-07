1bf070610e37bd0e6a64147e7d108227148e7633
Thomas Bateson <thomas.bateson@eagles.oc.edu>
2024-10-06 23:17:53 -0500

    Add jdk21 and jre21

--- src/pkgs/jre/jre21.ps1
+++ src/pkgs/jre/jre21.ps1
@@ -1,14 +1,14 @@
 $global:PwrPackageConfig = @{
 	Name = 'jre'
-	Matcher = '^jre-11\.'
+	Matcher = '^jre-21\.'
 }
 
 function global:Install-PwrPackage {
 	$Params = @{
 		Owner = 'adoptium'
-		Repo = 'temurin11-binaries'
+		Repo = 'temurin21-binaries'
 		AssetPattern = '^.*jre_x64_windows_hotspot_.+?\.zip$'
-		TagPattern = "^jdk-(11)\.([0-9]+)\.([0-9]+)((\.[0-9]+)?(\+[0-9]+)?)$"
+		TagPattern = "^jdk-(21)\.([0-9]+)\.([0-9]+)((\.[0-9]+)?(\+[0-9]+)?)$"
 	}
 	$Asset = Get-GitHubRelease @Params
 	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
@@ -24,14 +24,6 @@ function global:Install-PwrPackage {
 	Install-BuildTool @Params
 	New-Item -Path '\pkg\x64' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
 	Move-Item "$(Get-ChildItem -Path '\pkg-preinstall\x64' -Recurse -Include 'bin' | Select-Object -First 1 | ForEach-Object { Split-Path $_ })\*" '\pkg\x64'
-	$Params_x86 = @{
-		AssetName = $Asset.Name.Replace('_x64_', '_x86-32_')
-		AssetURL = $Asset.URL.Replace('_x64_', '_x86-32_')
-		ToolDir = '\pkg-preinstall\x86'
-	}
-	Install-BuildTool @Params_x86
-	New-Item -Path '\pkg\x86' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
-	Move-Item "$(Get-ChildItem -Path '\pkg-preinstall\x86' -Recurse -Include 'bin' | Select-Object -First 1 | ForEach-Object { Split-Path $_ })\*" '\pkg\x86'
 	Write-PackageVars @{
 		env = @{
 			java_home = (Split-Path (Get-ChildItem -Path '\pkg\x64' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
@@ -43,20 +35,11 @@ function global:Install-PwrPackage {
 				path = (Get-ChildItem -Path '\pkg\x64' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
 			}
 		}
-		x86 = @{
-			env = @{
-				java_home = (Split-Path (Get-ChildItem -Path '\pkg\x86' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
-				path = (Get-ChildItem -Path '\pkg\x86' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
-			}
-		}
 	}
 }
 
 function global:Test-PwrPackageInstall {
 	Airpower exec 'file:///\pkg' {
-		java -version
-	}
-	Airpower exec 'file:///\pkg<x86' {
 		java -version
 	}
 }
