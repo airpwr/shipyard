57024af3aa94b389d2fe4541ceaedbdf44a3b203
Troy Southard <troy.southard@us.af.mil>
2025-07-21 22:00:56 -0500

    Update obsidian asset pattern

--- src/pkgs/obsidian.ps1
+++ src/pkgs/obsidian.ps1
@@ -6,7 +6,7 @@ function global:Install-PwrPackage {
 	$Params = @{
 		Owner        = 'obsidianmd'
 		Repo         = 'obsidian-releases'
-		AssetPattern = '^Obsidian\.([0-9]+)\.([0-9]+)\.([0-9]+)\.exe$'
+		AssetPattern = '^Obsidian-\.([0-9]+)\.([0-9]+)\.([0-9]+)\.exe$'
 		TagPattern   = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
 	}
 	$Asset = Get-GitHubRelease @Params
