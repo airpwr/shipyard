6fae0f5ec41d575e58df2d049224206ed528cb8a
thomasgov <79868658+thomasgov@users.noreply.github.com>
2025-12-03 10:14:04 -0600

    Update nasm

--- src/pkgs/nasm.ps1
+++ src/pkgs/nasm.ps1
@@ -6,7 +6,7 @@ function global:Install-PwrPackage {
 	$Params = @{
 		Owner = 'netwide-assembler'
 		Repo = 'nasm'
-		TagPattern = '^.*([0-9]+)\.([0-9]+)\.([0-9]+)$'
+		TagPattern = '^nasm-([0-9]+)\.([0-9]+)(?:\.([0-9]+))?$'
 	}
 	$Latest = Get-GitHubTag @Params
 	$PwrPackageConfig.UpToDate = -not $Latest.Version.LaterThan($PwrPackageConfig.Latest)
