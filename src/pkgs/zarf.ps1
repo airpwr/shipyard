9bc44c487b8a9652995499a8a9f73479e8b6ac89
Troy Southard <troy.southard@us.af.mil>
2026-03-19 13:48:10 -0500

    Add zarf

---
+++ src/pkgs/zarf.ps1
@@ -0,0 +1,34 @@
+$global:PwrPackageConfig = @{
+	Name = 'zarf'
+}
+
+function global:Install-PwrPackage {
+	$Params = @{
+		Owner = 'zarf-dev'
+		Repo = 'zarf'
+		AssetPattern = 'zarf_v.+_Windows_amd64.exe'
+		TagPattern = '^v([0-9]+)\.([0-9]+)\.([0-9]+)$'
+	}
+	$Asset = Get-GitHubRelease @Params
+	$PwrPackageConfig.UpToDate = -not $Asset.Version.LaterThan($PwrPackageConfig.Latest)
+	$PwrPackageConfig.Version = $Asset.Version.ToString()
+	if ($PwrPackageConfig.UpToDate) {
+		return
+	}
+	$Params = @{
+		AssetName = 'zarf.exe'
+		AssetURL = $Asset.URL
+	}
+	Install-BuildTool @Params
+	Write-PackageVars @{
+		env = @{
+			path = '\pkg'
+		}
+	}
+}
+
+function global:Test-PwrPackageInstall {
+	airpower exec 'file:///\pkg' {
+		zarf version
+	}
+}
