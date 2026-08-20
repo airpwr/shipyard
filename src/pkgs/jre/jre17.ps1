e9c6bf8cf09fcbc48a23434d76b98398c8f46eb1
Nick Little <nicklaus.little@us.af.mil>
2026-08-20 06:58:23 -0500

    Removed x86 arch from JDK and JRE packages

--- src/pkgs/jre/jre17.ps1
+++ src/pkgs/jre/jre17.ps1
@@ -24,14 +24,6 @@ function global:Install-PwrPackage {
 	Install-BuildTool @Params
 	New-Item -Path '\pkg\x64' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
 	Move-Item "$(Get-ChildItem -Path '\pkg-preinstall\x64' -Recurse -Include 'bin' | Select-Object -First 1 | ForEach-Object { Split-Path $_ })\*" '\pkg\x64'
-	$Params_x86 = @{
-		AssetName = $Asset.Name.Replace('_x64_', '_x86-32_')
-		AssetURL = $Asset.URL.Replace('_x64_', '_x86-32_')
-		ToolDir = '\pkg-preinstall\x86'
-	}
-	Install-BuildTool @Params_x86
-	New-Item -Path '\pkg\x86' -ItemType Directory -Force -ErrorAction Ignore | Out-Null
-	Move-Item "$(Get-ChildItem -Path '\pkg-preinstall\x86' -Recurse -Include 'bin' | Select-Object -First 1 | ForEach-Object { Split-Path $_ })\*" '\pkg\x86'
 	Write-PackageVars @{
 		env = @{
 			java_home = (Split-Path (Get-ChildItem -Path '\pkg\x64' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
@@ -49,20 +41,11 @@ function global:Install-PwrPackage {
 				path = (Get-ChildItem -Path '\pkg\x64' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
 			}
 		}
-		x86 = @{
-			env = @{
-				java_home = (Split-Path (Get-ChildItem -Path '\pkg\x86' -Recurse -Include 'bin' | Select-Object -First 1).FullName -Parent)
-				path = (Get-ChildItem -Path '\pkg\x86' -Recurse -Include 'java.exe' | Select-Object -First 1).DirectoryName
-			}
-		}
 	}
 }
 
 function global:Test-PwrPackageInstall {
 	Airpower exec 'file:///\pkg' {
-		java -version
-	}
-	Airpower exec 'file:///\pkg<x86' {
 		java -version
 	}
 }
